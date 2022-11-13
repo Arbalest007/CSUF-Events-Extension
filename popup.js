aadd6c4561089b4122cebc8ec5f2fb0d717c29a6
Nicholas Jones <nicholasj898@gmail.com>
2022-11-13 02:27:22 -0800

    Update button fix

--- popup.js
+++ popup.js
@@ -4,16 +4,14 @@ let updateEvents = document.getElementById
 ('updateEvents');
 
 updateEvents.addEventListener("click", async () => {
-    alert('All CSUF events are up to date!');
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
+    location.reload();
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
         func: scrapeEventsFromCalendar,
     });
-})
-/*
-function scrapeEventsFromCalendar() {
     alert('All CSUF events are up to date!');
+})
+function scrapeEventsFromCalendar() {
+    
 }
-*/
