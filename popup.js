7939065ec142116c9836bc054bf1955685bc4b41
Nicholas Jones <nicholasj898@gmail.com>
2022-11-13 16:34:14 -0800

    Version Fix
    -fixed height/width of popout window
    -fixed update button not refreshing our new page
    -added "please wait" to the background image when calendar is updating

--- popup.js
+++ popup.js
@@ -4,16 +4,25 @@ let updateEvents = document.getElementById
 ('updateEvents');
 
 updateEvents.addEventListener("click", async () => {
-    alert('All CSUF events are up to date!');
+    
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
+    window.location.reload();
+    const delay = ms => new Promise(res => setTimeout(res, ms));
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
         func: scrapeEventsFromCalendar,
     });
+    await setTimeout(5000);
 })
-/*
-function scrapeEventsFromCalendar() {
-    alert('All CSUF events are up to date!');
+
+function secondFunction(){
+    // call first function and pass in a callback function which
+    // first function runs when it has completed
+    updateEvents(function() {
+        alert('All CSUF events are up to date!');
+    });    
 }
-*/
+
+function scrapeEventsFromCalendar() {
+    alert('Failed to update calendar.');
+}
