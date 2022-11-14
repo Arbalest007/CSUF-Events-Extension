ec68ca9456014d18f0434c0eeae782aea9b0f0e4
Nicholas Jones <nicholasj898@gmail.com>
2022-11-14 04:55:23 -0800

    Dark Theme
    -needs minor fixes
    -light theme not implemented

---
+++ darktheme.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  
+  var foobarElement = document.getElementById('foobar');
+  foobarElement.style.backgroundImage = '';
+  foobarElement.style.background = '';
+  foobarElement.style.backgroundUrl = '';
+  foobarElement.style.backgroundColor = '';
+
+  document.body.style.backgroundColor = 'black';
+
+});
+
