e5cfea3f94c6fd417479643bec48b3d9eef30742
Patrick Lin <pjlin@csu.fullerton.edu>
2022-11-15 21:37:27 -0800

    Update aboutscreen.js
    -Edited "About" message slightly

--- aboutscreen.js
+++ aboutscreen.js
@@ -18,7 +18,7 @@ let aboutInfo = document.getElementById("aboutInfo");
 
 aboutInfo.addEventListener("click", () => {
   alert(
-    "CPSC 254 project\nVersion 0.92\nMade by Sami Bajwa, Patrick Lin, and Nicholas Jones\nThis extension allows you to view previous and upcoming CSUF\
+    "CPSC 254 Semester Project\nVersion 0.92\nMade by Sami Bajwa, Patrick Lin, and Nicholas Jones\nThis extension allows you to view previous and upcoming CSUF\
     events all on one calendar, without needing to go to the official website."
   );
 });
